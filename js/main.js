6fe38b7041e577886fc7aa98013abf338f1a2bc6
Mateusz Tryniszewski <trynio132@gmail.com>
2018-10-31 13:51:21 +0100

    add scrollreveal

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ $(document).ready(function(){
         addScrolled();
         
     });
-    
-    
     function addScrolled(){
         
         if($(window).scrollTop() > $('.navbar').outerHeight()){
@@ -14,6 +12,43 @@ $(document).ready(function(){
            }else {
                $('.navbar').removeClass('change-color');
            }
-    }
+    };
     
-    
+ //
+
+ //scroll effect
+ $(document).ready(function() { 
+
+	$('a[href^="#"]').on('click', function(event) {
+	
+		var target = $( $(this).attr('href') );
+	
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 500);
+		}
+	});
+});
+//
+ window.sr = ScrollReveal();
+ sr.reveal('.flex-item', {
+     reset: false,
+     delay: 300,
+     distance: '100px',
+     origin: 'right'
+ });
+ sr.reveal('img,i,.about-content', {
+     reset: false,
+     delay: 500,
+     distance: '100px',
+     origin: 'right'
+ });
+ sr.reveal('p,h1,h2,h3,.col', {
+     reset: false,
+     delay: 500,
+     distance: '100px',
+     origin: 'right'
+ });
+ 
